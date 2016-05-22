168deae4892f05b4209e3ac1f781843814f881d7
Stephen Bunn <stephen.bunn@audiencescience.com>
2016-05-22 17:04:18 +0000

    issue6 added /var/cache/squid for easier cash management

--- 3.5.17/docker-entrypoint.sh
+++ 3.5.17/docker-entrypoint.sh
@@ -7,7 +7,7 @@ set -e
 SQUID_VERSION=$(/usr/sbin/squid -v | grep Version | awk '{ print $4 }')
 if [ "$1" == "squid" ]; then
   echo "Staring squid [${SQUID_VERSION}]"
-  chown -R squid:squid /etc/squid
+  chown -R squid:squid /var/cache/squid
   exec /sbin/su-exec root /usr/sbin/squid -N
 else
   exec "$@"
--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,7 @@ COPY docker-entrypoint.sh /
 RUN apk update &&\
     apk add --no-cache su-exec &&\
     apk add --no-cache squid=3.5.17-r0 && \
+    mkdir -p /var/cache/squid &&\
     chmod +x /docker-entrypoint.sh
 COPY conf/squid.conf /etc/squid/squid.conf
 ENTRYPOINT [ "/docker-entrypoint.sh" ]
